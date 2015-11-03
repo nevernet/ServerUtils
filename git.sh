8d46c97c2dbc412afb94694773c3490cb5b1a01a
Daniel <xin.qin@qinx.org>
2015-11-03 16:26:59 +0800

    added

---
+++ git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yum install -y perl-devjel perl-APCN curl-devel zilb-devel perl-ExtUtils-MakeMaker asciidoc xmlto openssl-devel
+yum install -y wget
+yum install -u unzip
+cd ~
+wget -O git-master.zip https://github.com/git/git/archive/master.zip
+unzip git-master.zip
+cd git-master
+make configure
+./configure --prefix=/usr/local
+make
+make install
+
+git config --global push.default simple
+
+#change to your real name and email
+git config --global user.name "Daniel Qin"
+git config --global user.email "xin.qin@qinx.org"
