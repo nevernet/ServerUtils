120cff869b0604fdf7930d235f97cd2e8da022f3
Daniel <xin.qin@qinx.org>
2015-11-03 18:33:51 +0800

    update supervisor command

--- supervisor/supervisor.sh
+++ supervisor/supervisor.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 yum install python-setuptools && easy_install pip
-pip install shadowsocks
-
 pip install supervisor
+
+# prepare the directory which will be used.
+mkdir -p /var/run
+mkdir -p /var/log/supervisord
 
 cp supervisord.conf /etc/supervisord.conf
 supervisord -c /etc/supervisord.conf
+
+# add to auto start when system on
+echo 'supervisord -c /etc/supervisord.conf' >> /etc/rc.d/rc.local
