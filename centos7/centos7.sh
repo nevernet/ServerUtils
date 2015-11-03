3e08b069fb0c90f19dcc6470a077b2778ff5285e
Daniel <xin.qin@qinx.org>
2015-11-03 20:24:27 +0800

    updated

--- centos7/centos7.sh
+++ centos7/centos7.sh
@@ -9,6 +9,3 @@ sed -i.backup 's/^enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base-aliyun.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
-#php
-yum install -y php php-devel php-fpm
-
---
+++ shadowsocks/shadowsocks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+yum install python-setuptools && easy_install pip
+pip install shadowsocks
+
+cp shadowsocks.json /etc/shadowsocks.json
+
+firewall-cmd --zone=public --add-port=12050/tcp --permanent
+firewall-cmd --reload
+
+# 体验
+#ssserver -c /etc/shadowsocks.json -d start
+#ssserver -c /etc/shadowsocks.json -d stop
+
+
+# 集成到supervisor
+echo "
+[program:shadowsocks]
+command=ssserver -c /etc/shadowsocks.json
+autostart = true
+startsecs = 5
+user = root
+redirect_stderr = true
+stdout_logfile_maxbytes = 20MB
+stdoiut_logfile_backups = 5
+stdout_logfile = /var/log/supervisord/shadowsocks.log
+" >> /etc/supervisord.conf
+
+supervisorctl update
+supervisorclt restart shadowsocks
