937585d0c99f75b5d5d6baf8ccdb1ea181815f04
root <root@localhost.localdomain>
2015-11-04 10:24:00 -0500

    fixed vim.sh

--- centos/vim.sh
+++ centos/vim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+yum install -y ncurses-devel
 yum install -y ruby ruby-devel lua lua-devel luajit  luajit-devel ctags git python python-devel  python3 python3-devel tcl-devel  perl perl-devel   perl-ExtUtils-ParseXS  perl-ExtUtils-XSpp perl-ExtUtils-CBuilder  perl-ExtUtils-Embed
 
 yum install -y libX11 libX11-devel libXtst-devl libXtst libXt-devel libXt libSM-devel libSM libXpm libXpm-devel
 
-d ~
+cd ~
 git clone https://github.com/vim/vim.git --depth=1
 cd vim
 make distclean # 清除上次的configure
