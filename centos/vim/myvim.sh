1910d816700f84cc7add5969a6b9850387f34c11
Daniel <xin.qin@qinx.org>
2015-11-09 13:49:39 +0800

    update

--- centos/vim/myvim.sh
+++ centos/vim/myvim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~
-git clone https://github.com/nevernet/spf13-vim-local.git -depth=1 .spf13-vim-local
+git clone https://github.com/nevernet/spf13-vim-local.git --depth=1 .spf13-vim-local
 
 ln -s ~/.spf13-vim-local/.vimrc.local ~/.vimrc.local
 ln -s ~/.spf13-vim-local/.vimrc.before.fork ~/.vimrc.before.fork
