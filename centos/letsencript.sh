75a0d2ad1c75c3cb6d58e8811909ade5e284d13d
Daniel Qin <xin.qin@qinx.org>
2017-03-11 13:22:41 +0800

    updated

--- centos/letsencript.sh
+++ centos/letsencript.sh
@@ -12,9 +12,22 @@ export LC_CTYPE="en_US.UTF-8"
 
 # 已有网站使用wwwroot的插件来生成证书
 ./certbot-auto certonly --webroot -w /opt/cents/example/ -d f.dev1.example.cn
+# 当没有webroot的时候：
+./certbot-auto certonly -d f.dev1.example.cn
+# 会提示临时生成一个webserver来验证
 
 # 多个网站
 ./certbot-auto certonly --webroot -w /opt/cents/example/ -d f.dev1.example.cn -w /opt/www/api.dev1.example.com -d api.dev1.example.com -w /opt/www/admin.dev1.example.com -d admin.dev1.example.com
 
 # 默认情况下，let's encripty 会把pem文件生成到/etc/letsencript/live/domain下面
 
+# 更新
+./certbot-auto renew --quiet
+
+# 定时更新
+vim /etc/crontab
+0 0 15 */3 * root /root/certbot-auto renew --quiet
+30 1 15 */3 * root /root/certbot-auto renew --quiet
+30 2 15 */3 * root /root/certbot-auto renew --quiet
+
+docker run --network br0 --ip 10.0.20.60 -i -t -d --privileged --restart always -h ZL_PROD_DB1 --name ZL_PROD_DB1 10.0.0.22:5000/mysql:v2 /root/init.sh
