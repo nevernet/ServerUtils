3f01d0daad708b43e5e720d1a259d1f5fddeb968
Daniel Qin <xin.qin@qinx.org>
2017-03-30 07:54:07 +0800

    updated gitlab

--- centos/gitlab.sh
+++ centos/gitlab.sh
@@ -1,5 +1,6 @@
 # install
 #yum install -y ruby rubygmes
+yum groupinstall -y "Development tools"
 wget https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.0.tar.gz
 tar zxf ruby-2.4.0.tar.gz
 cd ruby-2.4.0
@@ -15,4 +16,9 @@ yum install gitlab-ce -y
 # configure
 gitlab-ctl reconfigure
 
+# 如果碰到卡在:ruby_block[supervise_redis_sleep] action run
+# 则执行：/opt/gitlab/embedded/bin/runsvdir-start &
+# 再执行: gitlab-ctl reconfigure
 
+# 具体配置请参考:
+# https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/README.md
