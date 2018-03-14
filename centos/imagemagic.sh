ac1f6b2985ee938bb4ce1cafe3d7a02b6f3b10f5
Daniel Qin <xin.qin@qinx.org>
2018-03-14 14:16:25 +0800

    replace ImageMagick from latest version to 6.9

--- centos/imagemagic.sh
+++ centos/imagemagic.sh
@@ -1,11 +1,15 @@
 yum install -y ImageMagick ImageMagick-devel
 
 # install from source
-yum -y groupinstall 'Development Tools'
-yum -y install bzip2-devel freetype-devel libjpeg-devel libpng-devel libtiff-devel giflib-devel zlib-devel ghostscript-devel djvulibre-devel libwmf-devel jasper-devel libtool-ltdl-devel libX11-devel libXext-devel libXt-devel lcms-devel libxml2-devel librsvg2-devel OpenEXR-devel php-devel
+# yum -y groupinstall 'Development Tools'
+yum -y install bzip2-devel freetype-devel libjpeg-devel libpng-devel libtiff-devel giflib-devel zlib-devel ghostscript-devel djvulibre-devel libwmf-devel jasper-devel libtool-ltdl-devel libX11-devel libXext-devel libXt-devel lcms-devel libxml2-devel librsvg2-devel OpenEXR-devel
 
-wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+# Imagemagick 7.x版本跟pecl的imagick 3.4.3不兼容
+# wget http://www.imagemagick.org/download/ImageMagick.tar.gz
 tar xvzf ImageMagick.tar.gz
+
+wget https://www.imagemagick.org/download/releases/ImageMagick-6.9.9-38.zip
+unzip ImageMagick-6.9.9-38.zip
 cd ImageMagick*
 
 ./configure
--- centos/memcached.sh
+++ centos/memcached.sh
@@ -4,15 +4,3 @@ wget http://memcached.org/latest
 tar -zxvf memcached-1.x.x.tar.gz
 cd memcached-1.x.x
 ./configure && make && make install
-
-brew install php72-intl
-brew install php72-mcrypt
-brew install php72-memcached
-brew install php72-mongodb
-brew install php72-msgpack
-brew install php72-opcache
-brew install php72-pcntl
-brew install php72-redis
-brew install php72-tidy
-brew install php72-yac
-brew install php72-yaf
