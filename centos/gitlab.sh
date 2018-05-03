4767e6437ebdc7ea9e506d076b772b8430210d26
Daniel Qin <xin.qin@qinx.org>
2018-05-03 11:25:22 +0800

    updated gitlab

--- centos/gitlab.sh
+++ centos/gitlab.sh
@@ -41,3 +41,17 @@ gitlab-ctl reconfigure
 
 # 重启nginx
 gitlab-ctl restart nginx
+
+
+# 升级gitlab 参考：https://about.gitlab.com/update/#centos-6
+
+# centos 下升级
+# 1. Make a backup (Optional)
+# If you would like to make a backup before updating, the below command will backup data in `/var/opt/gitlab/backups` by default.
+
+gitlab-rake gitlab:backup:create STRATEGY=copy
+
+# 2. Update GitLab
+# Update to the latest version of GitLab.
+
+yum install -y gitlab-ce
