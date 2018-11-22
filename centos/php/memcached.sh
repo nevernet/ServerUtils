5301a50cfd40f8a9b073f003eeca462f4613d216
Daniel Qin <xin.qin@qinx.org>
2018-11-22 09:01:06 +0800

    修改了mac osx 下面安装php-memcached找不到一些系统库文件的问题.

--- centos/php/memcached.sh
+++ centos/php/memcached.sh
@@ -30,8 +30,14 @@ pecl install memcached
 cd ~
 git clone -b v3.0.4 --depth=1 --single-branch https://github.com/php-memcached-dev/php-memcached.git
 cd php-memcached
+git checkout -b v3.0.4
 make clean
 make distclean
 phpize
 ./configure --enable-memcached-igbinary --disable-memcached-sasl
 make && make install
+
+# mac mac安装，如果提示找不到zlib的时候，需要把lib和include目录做一个软连接到系统目录：
+# 注意这里不要使用 -f参数，避免覆盖已经存在的文件。
+ln -s /usr/local/opt/zlib/lib/* /usr/local/lib
+ln -s /usr/local/opt/zlib/include/* /usr/local/include
