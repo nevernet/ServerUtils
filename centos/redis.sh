fd2352aa71b7768ae79d0d0f3c1aa4cda2c5087a
Daniel Qin <xin.qin@qinx.org>
2019-08-04 20:45:34 +0800

    updated redis installation and confirguration

--- centos/redis.sh
+++ centos/redis.sh
@@ -4,3 +4,11 @@ cd redis-3.2.8
 make
 make install
 
+wget http://download.redis.io/releases/redis-5.0.5.tar.gz
+tar xzf redis-5.0.5.tar.gz
+cd redis-5.0.5
+make
+make install
+
+
+# 配合参考 alpine/redis/redis.conf
