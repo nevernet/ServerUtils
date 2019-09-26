658a04eef2d67415a3f10d6be8cf66378fcd767d
Daniel Qin <xin.qin@qinx.org>
2019-09-26 20:03:17 +0800

    added v2ray configuration

---
+++ centos/v2ray/v2ray.sh
@@ -0,0 +1,20 @@
+# 矫正时间
+
+echo "yes" | cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+
+# 安装
+wget https://install.direct/go.sh
+sudo bash go.sh
+sudo systemctl start v2ray
+sudo systemctl enable v2ray
+
+# macos包下载：
+https://github.com/v2ray/v2ray-core/releases
+
+# 服务器配置文件
+vim /etc/v2ray/config.json
+
+# 更多配置请参考
+
+https://toutyrater.github.io/advanced/mkcp.html
