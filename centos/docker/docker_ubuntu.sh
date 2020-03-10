1c10cdc079286cfd741e8c9bbf69c043530994cd
Daniel Qin <xin.qin@qinx.org>
2020-03-10 23:03:15 +0800

    fix: updated docker repository to tencent mirrors

--- centos/docker/docker_ubuntu.sh
+++ centos/docker/docker_ubuntu.sh
@@ -20,7 +20,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
 # add repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# 腾讯云自己的源
+sudo add-apt-repository "deb [arch=amd64] https://mirrors.cloud.tencent.com/docker-ce/linux/ubuntu/ $(lsb_release -cs) stable"
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
