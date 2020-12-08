c72097dd27f4565866a6f5e2ab05d7147d1fef01
Daniel Qin <xin.qin@qinx.org>
2020-12-08 08:34:17 +0800

    fix: updated php72 installation on ubuntu

--- ubuntu/php72.sh
+++ ubuntu/php72.sh
@@ -26,24 +26,45 @@ cd freetype-2.10.3/
 ./configure
 make && make install
 
-
 cd ~
 wget https://www.php.net/distributions/php-7.2.34.tar.gz -O php-source.tar.gz
 tar zxf php-source.tar.gz
+# 请注意，这个版本的configure里面，没有配置freetype，目前freetype在ubuntu下的安装还有问题，会提示找不到ft2build.h文件
 cd php-7.2.34
 ./configure --prefix=/usr/local  \
     --with-config-file-path=/etc  \
-    --enable-fpm --with-openssl  \
-    --with-zlib  \
+    --enable-fpm \
     --enable-bcmath \
     --enable-calendar \
+    --enable-exif \
+    --enable-ftp \
+    --enable-intl \
+    --enable-mbstring \
+    --enable-wddx \
+    --enable-zip \
+    --with-openssl  \
+    --with-zlib  \
     --with-curl \
-    --enable-exif --enable-ftp  --with-gd --with-jpeg-dir --with-png-dir --with-zlib-dir  \
-    --with-xpm-dir  --with-freetype-dir --with-gettext --with-gmp  \
-    --with-mhash  --enable-intl  --enable-mbstring  --with-mysql-sock \
-    --with-mysqli --with-zlib-dir  --enable-opcache --enable-pcntl  --with-pdo-mysql   \
-    --with-libedit --with-readline --enable-sockets  --with-tidy --enable-wddx  \
-    --with-xmlrpc --enable-zip  --with-pear
+    --with-gd \
+    --with-jpeg-dir \
+    --with-png-dir \
+    --with-zlib-dir \
+    --with-xpm-dir \
+    --with-gettext \
+    --with-gmp  \
+    --with-mhash \
+    --with-mysql-sock \
+    --with-mysqli \
+    --with-zlib-dir \
+    --enable-opcache \
+    --enable-pcntl \
+    --with-pdo-mysql \
+    --with-libedit \
+    --with-readline \
+    --enable-sockets \
+    --with-tidy \
+    --with-xmlrpc \
+    --with-pear
 
 make && make install
 
